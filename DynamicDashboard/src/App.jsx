b068d625e43529248cfdb408d65e4336993ad3ff
fedallxn <faithe.dall@gmail.com>
2026-02-14 14:37:53 -0600

    Creating a dynamic greeting to welcome the user with today's date displayed underneath! Altered hoe the day would be displayed because I wanted to include the weekday and the month to be spelled out insted of in it's default numerical form!

--- DynamicDashboard/src/App.jsx
+++ DynamicDashboard/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import DynamicGreeting from './components/DynamicGreeting';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const todaysDate = new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  });
+  const userName = "Faith";
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <DynamicGreeting name={userName}/>
+        <p>{todaysDate}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
