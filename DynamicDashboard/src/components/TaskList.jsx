b16920c84db22c9b4715617ccec1131e2e106d37
fedallxn <faithe.dall@gmail.com>
2026-02-15 10:32:23 -0600

    Added another element to the taskList feature, it showcases which tasks are completed vs not completed! I think I'm going to leave the CSS alone for know otherwise I'd be working on this FOREVER!

--- DynamicDashboard/src/components/TaskList.jsx
+++ DynamicDashboard/src/components/TaskList.jsx
@@ -1,11 +1,17 @@
 const DisplayList = ({tasks}) => {
+    const completedTasks = tasks.filter(task => task.isCompleted).length; //filters the number of tasks that are completed
+    const incompleteTasks = tasks.filter(task => !task.isCompleted).length; //filters the number of tasks thar are incomplete
+    //THIS ONE GAVE ME SUCH A HARD TIME BC I FORGOT TO PUT A D AFTER COMPLETE! It kept syaing I have 5 incomplete tasks, but I'm glad I found it!
     return (
-        <ul className="listOfTasks">
-            {tasks.map((task) => (
-                <li key={task.taskName}>
-                    {task.isCompleted ? "✅" : "❌"}{task.taskName}
-                </li>))}
-        </ul>
+        <>
+            <ul className="listOfTasks">
+                {tasks.map((task) => (
+                    <li key={task.taskName}>
+                        {task.isCompleted ? "✅" : "❌"}{task.taskName}
+                    </li>))}
+            </ul>
+            <p>Completed Tasks: {completedTasks} | Incomplete Tasks: {incompleteTasks}</p>
+        </>
     )
 }
 
